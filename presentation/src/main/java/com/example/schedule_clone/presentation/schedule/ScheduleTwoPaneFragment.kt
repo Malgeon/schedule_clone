65d67de650214454fd28d386ba2a44e04b2ba51d
malgeon <nyangsun@gmail.com>
2022-01-31 15:19:57 +0900

    add - drawable 추가

---
+++ presentation/src/main/java/com/example/schedule_clone/presentation/schedule/ScheduleTwoPaneFragment.kt
@@ -0,0 +1,19 @@
+package com.example.schedule_clone.presentation.schedule
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.schedule_clone.presentation.R
+
+class ScheduleTwoPaneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule_two_pane, container, false)
+    }
+}
