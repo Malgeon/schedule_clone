a633498380b2f9ec83660e5e38614044f4320741
Malgeon <nyangsun@gmail.com>
2022-02-09 18:02:03 +0900

    modify - fragment hilt annotation 추가

--- presentation/src/main/java/com/example/schedule_clone/presentation/speaker/SpeakerFragment.kt
+++ presentation/src/main/java/com/example/schedule_clone/presentation/speaker/SpeakerFragment.kt
@@ -6,8 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.schedule_clone.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class SpeakerFragment : Fragment() {
 
     override fun onCreateView(
