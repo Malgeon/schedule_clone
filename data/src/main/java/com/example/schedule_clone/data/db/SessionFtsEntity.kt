52984a3ec7b3a0dd230725efdb06b37856e04b8f
Malgeon <nyangsun@gmail.com>
2022-02-18 12:06:05 +0900

    add - session entity 및 dao 추가

--- data/src/main/java/com/example/schedule_clone/data/db/SessionFtsEntity.kt
+++ data/src/main/java/com/example/schedule_clone/data/db/SessionFtsEntity.kt
@@ -1,6 +1,8 @@
 package com.example.schedule_clone.data.db
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Fts4
 import com.example.schedule_clone.model.Session
 
 /**
@@ -10,5 +12,39 @@ import com.example.schedule_clone.model.Session
  * properties without requiring changing the column name.
  */
 @Entity(tableName = "sessionFts")
-class SessionFtsEntity {
-}
+@Fts4
+class SessionFtsEntity(
+
+    /**
+     * An FTS entity table always has a column named rowid that is the equivalent of an
+     * INTEGER PRIMARY KEY index. Therefore, an FTS entity can only have a single field
+     * annotated with PrimaryKey, it must be named rowid and must be of INTEGER affinity.
+     *
+     * The field can be optionally omitted in the class (as is done here),
+     * but can still be used in queries.
+     */
+
+    /**
+     * Unique string identifying this session.
+     */
+    @ColumnInfo(name = "sessionId")
+    val sessionId: String,
+
+    /**
+     * Session title.
+     */
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    /**
+     * Body of text with the session's description.
+     */
+    @ColumnInfo(name = "description")
+    val description: String,
+
+    /**
+     * The session speaker(s), stored as a CSV String.
+     */
+    @ColumnInfo(name = "speakers")
+    val speakers: String
+)
