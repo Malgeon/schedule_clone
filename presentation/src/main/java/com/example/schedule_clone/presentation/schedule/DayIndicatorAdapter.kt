f74040c9b62a6a41be7d5633927e4fc74c45c7af
Malgeon <nyangsun@gmail.com>
2022-02-23 14:21:41 +0900

    modify - indicator adapter

---
+++ presentation/src/main/java/com/example/schedule_clone/presentation/schedule/DayIndicatorAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.schedule_clone.presentation.schedule
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.schedule_clone.presentation.databinding.ItemScheduleDayIndicatorBinding
+import com.example.schedule_clone.presentation.util.executeAfter
+import com.example.schedule_clone.shared.util.TimeUtils
+
+class DayIndicatorAdapter(
+    private val scheduleViewModel: ScheduleViewModel,
+    private val lifecycleOwner: LifecycleOwner
+) : ListAdapter<DayIndicator, DayIndicatorViewHolder>(IndicatorDiff) {
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return getItem(position).day.start.toEpochSecond()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayIndicatorViewHolder {
+        val binding = ItemScheduleDayIndicatorBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return DayIndicatorViewHolder(binding, scheduleViewModel, lifecycleOwner)
+    }
+
+    override fun onBindViewHolder(holder: DayIndicatorViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class DayIndicatorViewHolder(
+    private val binding: ItemScheduleDayIndicatorBinding,
+    private val scheduleViewModel: ScheduleViewModel,
+    private val lifecycleOwner: LifecycleOwner
+) : ViewHolder(binding.root) {
+
+    fun bind(item: DayIndicator) {
+        binding.executeAfter {
+            indicator = item
+            viewModel = scheduleViewModel
+            lifecycleOwner = this@DayIndicatorViewHolder.lifecycleOwner
+        }
+    }
+}
+
+object IndicatorDiff : ItemCallback<DayIndicator>() {
+    override fun areItemsTheSame(oldItem: DayIndicator, newItem: DayIndicator) =
+        oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: DayIndicator, newItem: DayIndicator) =
+        oldItem.areUiContentsTheSame(newItem)
+}
+
+@BindingAdapter("indicatorText", "inConferenceTimeZone", requireAll = true)
+fun setIndicatorText(
+    view: TextView,
+    dayIndicator: DayIndicator,
+    inConferenceTimeZone: Boolean
+) {
+    view.setText(TimeUtils.getShortLabelResForDay(dayIndicator.day, inConferenceTimeZone))
+}
