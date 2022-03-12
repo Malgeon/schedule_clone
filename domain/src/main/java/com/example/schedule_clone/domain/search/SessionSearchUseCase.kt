52015cbc6dc17fe23bbf776f56cc420c4d1d7c75
malgeon <nyangsun@gmail.com>
2022-03-12 21:47:52 +0900

    modify - session search use case 수정

--- domain/src/main/java/com/example/schedule_clone/domain/search/SessionSearchUseCase.kt
+++ domain/src/main/java/com/example/schedule_clone/domain/search/SessionSearchUseCase.kt
@@ -1,7 +1,13 @@
 package com.example.schedule_clone.domain.search
 
+import com.example.schedule_clone.domain.FlowUseCase
 import com.example.schedule_clone.domain.userevent.SessionAndUserEventRepository
 import com.example.schedule_clone.model.filters.Filter
+import com.example.schedule_clone.model.userdata.UserSession
+import com.example.schedule_clone.shared.di.IoDispatcher
+import com.example.schedule_clone.shared.result.Result
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 data class SessionSearchUseCaseParams(
@@ -13,6 +19,10 @@ data class SessionSearchUseCaseParams(
 class SessionSearchUseCase @Inject constructor(
     private val repository: SessionAndUserEventRepository,
     private val textMatchStrategy: SessionTextMatchStrategy,
+    @IoDispatcher dispatcher: CoroutineDispatcher
+) : FlowUseCase<SessionSearchUseCaseParams, List<UserSession>>(dispatcher) {
 
-){
+    override fun execute(parameters: SessionSearchUseCaseParams): Flow<Result<List<UserSession>>> {
+        TODO("Not yet implemented")
+    }
 }
