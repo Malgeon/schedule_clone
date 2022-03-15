52077efd8bc1cb9f0999cc347c2481db743cfce9
Malgeon <nyangsun@gmail.com>
2022-03-15 18:10:15 +0900

    add - filter fragment 추가

---
+++ presentation/src/main/java/com/example/schedule_clone/presentation/filters/FiltersFragment.kt
@@ -0,0 +1,14 @@
+package com.example.schedule_clone.presentation.filters
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Fragment that shows the list of filters for teh Schedule
+ */
+abstract class FiltersFragment : Fragment() {
+
+    companion object {
+        // Threshold for when the filter sheet content should become invisible.
+        // This should be a value between 0 and 1, coinciding
+    }
+}
