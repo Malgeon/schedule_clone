b796bd94813d7aef8daa2005d43ef5e5053a32a8
Malgeon <nyangsun@gmail.com>
2022-03-21 18:05:08 +0900

    modify - selectable filter chip adapter 로직 수정

--- presentation/src/main/java/com/example/schedule_clone/presentation/filters/SelectableFilterChipAdapter.kt
+++ presentation/src/main/java/com/example/schedule_clone/presentation/filters/SelectableFilterChipAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.schedule_clone.presentation.filters
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.schedule_clone.presentation.R
+import com.example.schedule_clone.presentation.SectionHeader
+import com.example.schedule_clone.shared.util.exceptionInDebug
+import java.lang.IllegalArgumentException
+import java.lang.RuntimeException
 
 /** Adapter for selectable filters, e.g. ones shown in the filter sheet. */
 class SelectableFilterChipAdapter(
@@ -17,7 +21,52 @@ class SelectableFilterChipAdapter(
 
         /**
          * Inserts category headings in a list of [FilterChip]s to make a heterogeneous list.
+         * Assumes the items are already grouped by [FilterChip.categoryLabel], beginning with
+         * categoryLabel == '0'
          */
+        private fun insertCategoryHeadings(list: List<FilterChip>?): List<Any> {
+            val newList = mutableListOf<Any>()
+            var previousCategory = 0
+            list?.forEach {
+                val category = it.categoryLabel
+                if (category != previousCategory && category != 0) {
+                    newList += SectionHeader(
+                        titleId = category,
+                        useHorizontalPadding = false
+                    )
+                }
+                newList.add(it)
+                previousCategory = category
+            }
+            return newList
+        }
+    }
+
+    override fun submitList(list: MutableList<Any>?) {
+        exceptionInDebug(
+            RuntimeException("call `submitEventFilterList()` instead to add category headings.")
+        )
+        super.submitList(list)
+    }
+
+    /** Prefer this method over [submitList] to add category headings. */
+    fun submitFilterList(list: List<FilterChip>?) {
+        super.submitList(insertCategoryHeadings(list))
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is SectionHeader -> VIEW_TYPE_HEADING
+            is FilterChip -> VIEW_TYPE_FILTER
+            else -> throw IllegalArgumentException("Unknown item type")
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_HEADING -> createHeadingViewHolder()
+
+        }
     }
 }
 
