d668bab7583813595426184f529d433b49660b4e
malgeon <nyangsun@gmail.com>
2022-03-22 21:45:13 +0900

    modify - search View Model 수정

--- presentation/src/main/java/com/example/schedule_clone/presentation/search/SearchViewModel.kt
+++ presentation/src/main/java/com/example/schedule_clone/presentation/search/SearchViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.schedule_clone.presentation.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.schedule_clone.domain.search.LoadSearchFiltersUseCase
 import com.example.schedule_clone.domain.search.SessionSearchUseCase
 import com.example.schedule_clone.domain.settings.GetTimeZoneUseCase
@@ -8,9 +9,15 @@ import com.example.schedule_clone.model.userdata.UserSession
 import com.example.schedule_clone.presentation.filters.FiltersViewModelDelegate
 import com.example.schedule_clone.presentation.signin.SignInViewModelDelegate
 import com.example.schedule_clone.shared.analytics.AnalyticsHelper
+import com.example.schedule_clone.shared.result.Result
+import com.example.schedule_clone.shared.result.Result.Loading
+import com.example.schedule_clone.shared.result.successOr
+import com.example.schedule_clone.shared.util.TimeUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import org.threeten.bp.ZoneId
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,5 +37,64 @@ class SearchViewModel @Inject constructor(
 
     private val _isEmpty = MutableStateFlow(true)
     val isEmpty: StateFlow<Boolean> = _isEmpty
+
+    private var searchJob: Job? = null
+
+    val timeZoneId: StateFlow<ZoneId> = flow {
+        if (getTimeZoneUseCase(Unit).successOr(true)) {
+            emit(TimeUtils.CONFERENCE_TIMEZONE)
+        } else {
+            emit(ZoneId.systemDefault())
+        }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, ZoneId.systemDefault())
+
+    private var textQuery = ""
+
+    // Override because we also want to show result count when there's a text query.
+    private val _showResultCount = MutableStateFlow(false)
+    override val showResultCount: StateFlow<Boolean> = _showResultCount
+
+    init {
+        // Load filters
+        viewModelScope.launch {
+            setSupportedFilters(loadFilterUseCase(Unit).successOr(emptyList()))
+        }
+
+        // Re-execute search when selected filters change
+        viewModelScope.launch {
+            selectedFilters.collect {
+                executeSearch()
+            }
+        }
+    }
+
+    private fun executeSearch() {
+        // Cancel any in-flight searches
+        searchJob?.cancel()
+
+        val filters = selectedFilters.value
+        if (textQuery.isEmpty() && filters.isEmpty()) {
+            clearSearchResults()
+        }
+    }
+
+    private fun clearSearchResults() {
+        _searchResults.value = emptyList()
+        // Explicitly set false to not show the "No results" state
+        _isEmpty.value = false
+        _showResultCount.value = false
+        resultCount.value = 0
+    }
+
+    private fun processSearchResult(searchResult: Result<List<UserSession>>) {
+        if (searchResult is Loading) {
+            return // avoid UI flickering
+        }
+        val sessions = searchResult.successOr(emptyList())
+        _searchResults.value = sessions
+        _isEmpty.value = sessions.isEmpty()
+        _showResultCount.value = true
+        resultCount.value = sessions.size
+    }
 
 }
